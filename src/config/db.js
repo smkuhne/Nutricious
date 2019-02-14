d2fea7ea017a3cfb0d49edb429e7ec01bb62070d
smkuhne <43682152+smkuhne@users.noreply.github.com>
2019-02-13 17:42:25 -0800

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -1,12 +1,7 @@
 import Firebase from 'firebase';
 
 let config = {
-    apiKey: "AIzaSyAM2MqZydUEB0HE5RbC1hfdKRT8VHVmzeY",
-    authDomain: "driven-actor-231302.firebaseapp.com",
-    databaseURL: "https://driven-actor-231302.firebaseio.com",
-    projectId: "driven-actor-231302",
-    storageBucket: "driven-actor-231302.appspot.com",
-    messagingSenderId: "1020454092376"
+    //PrivateInfo
 };
 
 let app = Firebase.initializeApp(config);
